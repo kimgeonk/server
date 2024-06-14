5f1b6dadb8b995236e6fb490daf9860774928236
kimgeonk <162672654+kimgeonk@users.noreply.github.com>
2024-06-14 22:44:32 +0900

    final2

--- 시계지점/강변북로_난지공원.py
+++ 시계지점/강변북로_난지공원.py
@@ -66,4 +66,4 @@ async def read_traffic():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='localhost', port=8080, log_level="debug")
+    uvicorn.run(app, host='127.0.0.1', port=8080, log_level="debug")
